788add054852ad6d31e56e206ea582e8d2aa3f8d
bptan <t.boonpiao@gmail.com>
2016-08-05 13:59:35 +0800

    restappconfig

---
+++ src/java/sa42/uno/rest/RESTAppConfig.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sa42.uno.rest;
+
+import javax.ws.rs.ApplicationPath;
+
+/**
+ *
+ * @author BP
+ */
+@ApplicationPath("api")
+public class RESTAppConfig {
+    
+}
