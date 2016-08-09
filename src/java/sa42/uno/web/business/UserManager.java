2d562ccd91a05dc48b5872a764b23c05680be46e
bptan <t.boonpiao@gmail.com>
2016-08-09 14:31:27 +0800

    before adding persistence unit

--- src/java/sa42/uno/web/business/UserManager.java
+++ src/java/sa42/uno/web/business/UserManager.java
@@ -5,7 +5,12 @@
  */
 package sa42.uno.web.business;
 
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import sa42.uno.web.model.Users;
 
 /**
  *
@@ -13,13 +18,38 @@ import javax.ejb.Stateless;
  */
 @Stateless
 public class UserManager {
-    
-    private static final String INSERT_USER = "insert u from authdb.users";
-    
-    public int registerUser(String username, String password){
-        
-        
+
+    public static final String GROUP = "NormalUser";
+    private static final String FIND_USER
+            = "select u from users u where u.userid = :username";
+
+    private static final String REGISTER_USER = "insert u from users u";
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @EJB
+    private GroupManager groupMgr;
+
+    private int checkUsernameInUse(String username) {
+
         return 0;
     }
-    
+
+    public Boolean registerUser(String username, String password) {
+        Users u = new Users();
+        u.setUsername(username);
+        u.setPassword(password);
+        try{
+            em.persist(u);
+            groupMgr.addToGroup(GROUP, username);
+            em.getEntityManagerFactory().getCache()
+				.evict(Users.class, u.getUsername());
+            return true;
+        }catch(EntityExistsException ex){
+            return false;
+        }        
+        
+    }
+
 }
---
+++ src/java/sa42/uno/web/model/Groups.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sa42.uno.web.model;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author BP
+ */
+@Entity
+@Table(name = "groups")
+public class Groups implements Serializable{
+    
+    @Id
+    @Column(name="groupid")
+    private String groupId;
+    
+    @Id
+    private String userid;
+    
+    @OneToMany(mappedBy = "groups")
+    private List<Users> users;
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+}
